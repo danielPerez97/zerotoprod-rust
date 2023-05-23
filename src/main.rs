79723e8f4cdbc678e7c2290c060f7724bfc2420b
Daniel Perez <doperez1997@gmail.com>
2023-05-23 15:17:20 -0500

    Split the project out into multiple files and add a subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zerotoprod::startup::run;
+use zerotoprod::configuration::get_configuration;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
