9860fd5d20361b36580b36c9d34d3015f96ad6fd
Daniel Perez <doperez1997@gmail.com>
2023-05-23 21:43:47 -0500

    Changes to how the application is started and configured for testing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
+use sqlx::PgPool;
 use zerotoprod::startup::run;
 use zerotoprod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
